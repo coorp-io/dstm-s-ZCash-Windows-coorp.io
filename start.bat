c484638e73ea90976533d0a9073b3910ecb06942
coorp.io <post@coorp.io>
2018-02-04 20:23:54 +0300

    fix

--- start.bat
+++ start.bat
@@ -1,18 +1,24 @@
 echo Don't forget to change your pool and login information.
 
-#zm --server us.coorp.io --user [address].[worker] --pass x --port 3034
-#ZenCash
-./zm --server eu.coorp.io --user znSNxcGBUZSuhiR3JHtHKygGi35VXgAgdPe.workerName --pass x --port 3032
-#hush
-#./zm --server us.coorp.io --user t1RGwDc21mxd9J511GkQng78j8aFrZPJa3A.workerName --pass x --port 3034
-#VoteCoin
-#./zm --server us.coorp.io --user t1MskC7ubCKUDixmbYL7cLHvdkNJg5aDXpd.workerName --pass x --port 3035
-#BitcoinZ
-#./zm --server us.coorp.io --user t1gUFb4za5jLvpjz7iRc3hPPcFySMdtkKYv.workerName --pass x --port 3033
-#SnowGem
-#./zm --server us.coorp.io --user [address].[worker] --pass x --port 3031
-#Komodo
-#./zm --server eu.coorp.io --user RSCMuFshgvVnf6X4ukVgBHv9zS1sUdr2Ld.workerName --pass x --port 3857
+zm --server [server] --port [port] --user [address].[worker] --pass x
+
+#Hush https://coorp.io/pool/hush
+#zm --server us.coorp.io --port 3034 --user t1RGwDc21mxd9J511GkQng78j8aFrZPJa3A.workerName --pass x
+
+#Votecoin https://coorp.io/pool/vot
+#zm --server us.coorp.io --port 3035 --user t1MskC7ubCKUDixmbYL7cLHvdkNJg5aDXpd.workerName --pass x
+
+#BitcoinZ https://coorp.io/pool/btcz
+#zm --server us.coorp.io --port 3033 --user t1gUFb4za5jLvpjz7iRc3hPPcFySMdtkKYv.workerName --pass x
+
+#SnowGem https://coorp.io/pool/sng
+#zm --server us.coorp.io --port 3031 --user s1cem8nZGpeDRqUBsYspFUxwJpxbd1JrFAV.workerName --pass x
+
+#Komodo https://coorp.io/pool/kmd
+#zm --server eu.coorp.io --port 3857 --user RSCMuFshgvVnf6X4ukVgBHv9zS1sUdr2Ld.workerName --pass x
+
+#ZenCash https://coorp.io/pool/zen
+#zm --server eu.coorp.io --port 3032 --user znSNxcGBUZSuhiR3JHtHKygGi35VXgAgdPe.workerName --pass x
 
 
 @echo off
